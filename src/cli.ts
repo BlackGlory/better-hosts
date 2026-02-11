c7f737b61b352804db11cbbf8d52329c8ba08793
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-11 13:48:00 +0000

    fix(cli): timeout

--- src/cli.ts
+++ src/cli.ts
@@ -57,7 +57,7 @@ function getPort(options: IOptions): number {
 function getTimeout(options: IOptions): number {
   assert(/^\d+$/.test(options.timeout), 'The parameter timeout must be integer')
 
-  return Number.parseInt(options.port, 10) * 1000
+  return Number.parseInt(options.timeout, 10) * 1000
 }
 
 function getFallbackServer(options: IOptions): IServerInfo {
